c539ba6562535aa82fdd614c82eb96bdf63b16ad
evans kelechi <evanskcee@gmail.com>
2020-04-08 02:51:30 -0700

    Add project files.

---
+++ watch/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace watch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopstart = new Stopstart();
+
+            while(true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter 'start' to start stopwatch\n\n Enter 'stop' to end stopwatch\n\n press any key to stop");
+                    var input = Console.ReadLine().ToLower();
+
+                    if (input == "start")
+                    { stopstart.Start(); }
+                    else if (input == "stop")
+                    { stopstart.Stop();
+                        Console.WriteLine("Duration: {0}", stopstart.Getduration());
+                    }
+
+                    else
+                        break;
+
+                }
+                catch(InvalidOperationException)
+                {
+                    Console.WriteLine("stopwatch is in motion");
+                }
+            }
+
+        }
+
+
+        
+
+
+    }
+}
+
+
+
---
+++ watch/stopstart.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace watch
+{
+    class Stopstart
+    {
+        private DateTime _start;
+        private DateTime _stop;
+        private bool _isrunning;
+
+
+        public void Start()
+        {
+                if(_isrunning)
+                throw new InvalidOperationException("stopwatch is already running");
+
+            _start = DateTime.Now;
+            _isrunning = true;
+        }
+
+        public void Stop()
+        {
+            if (!_isrunning)
+                throw new InvalidOperationException("stopwatch has stopped already");
+
+            _stop = DateTime.Now;
+            _isrunning = false;
+        }
+
+        public TimeSpan Getduration()
+        {
+            return _start - _stop;
+        }
+    }
+}
